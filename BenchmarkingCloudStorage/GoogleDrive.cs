691db880d8db62849dc029ceb1ce029d2cd8e2a1
Karmen <karmen.gostisa@gmail.com>
2017-04-10 21:24:03 +0200

    Added load generating Created interface and derived GoogleDrive

---
+++ BenchmarkingCloudStorage/GoogleDrive.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using Microsoft.Win32;
+using File = Google.Apis.Drive.v3.Data.File;
+
+namespace BenchmarkingCloudStorage
+{
+    class GoogleDrive : IClouds
+    {
+        private DriveService _service;
+        
+        // If you modify these scopes, delete your previously saved credentials at ~/.credentials/drive-dotnet-quickstart.json
+        // Or it won't work :)
+        static string[] Scopes = { DriveService.Scope.Drive,
+                           DriveService.Scope.DriveAppdata,
+                           DriveService.Scope.DriveFile,
+                           DriveService.Scope.DrivePhotosReadonly,
+                           DriveService.Scope.DriveMetadataReadonly,
+                           DriveService.Scope.DriveReadonly,
+                           DriveService.Scope.DriveScripts };
+        static string ApplicationName = "Benchmarking Cloud Storage";
+
+        public void StartService()
+        {
+            // Create user credentials.
+            UserCredential credential;
+
+            using (var stream = new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+                credPath = Path.Combine(credPath, ".credentials/drive-dotnet-quickstart.json");
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Drive API service.
+            _service = new DriveService(new BaseClientService.Initializer
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName
+            });
+        }
+        
+        public void UploadFile(string filePath)
+        {
+            if (System.IO.File.Exists(filePath))
+            {
+                File body = new File();
+                body.Name = Path.GetFileName(filePath);
+                body.Description = "Test upload";
+                body.MimeType = GetMimeType(filePath);
+
+                // File's content. 
+                byte[] byteArray = System.IO.File.ReadAllBytes(filePath);
+                MemoryStream stream = new MemoryStream(byteArray);
+
+                try
+                {
+                    FilesResource.CreateMediaUpload request = _service.Files.Create(body, stream, GetMimeType(filePath));
+                    request.Upload();
+                    return;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("An error occurred: " + e.Message);
+                    return;
+                }
+            }
+            Console.WriteLine("File does not exist: " + filePath);
+        }
+
+        // Get the mime type of the file. 
+        private string GetMimeType(string fileName)
+        {
+            string mimeType = "application/unknown";
+            string ext = Path.GetExtension(fileName)?.ToLower();
+            RegistryKey regKey = Registry.ClassesRoot.OpenSubKey(ext);
+
+            if (regKey != null && regKey.GetValue("Content Type") != null)
+                mimeType = regKey.GetValue("Content Type").ToString();
+
+            return mimeType;
+        }
+
+        public void ListFiles()
+        {
+            FilesResource.ListRequest listRequest = _service.Files.List();
+            listRequest.PageSize = 10;
+            listRequest.Fields = "nextPageToken, files(id, name)";
+
+            IList<File> files = listRequest.Execute().Files;
+
+            Console.WriteLine("All files:");
+
+            if (files != null && files.Count > 0)
+            {
+                foreach (var f in files)
+                {
+                    Console.WriteLine("{0} ({1})", f.Name, f.Id);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No files found.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
--- BenchmarkingCloudStorage/Program.cs
+++ BenchmarkingCloudStorage/Program.cs
@@ -1,132 +1,62 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Threading;
-using Google.Apis.Auth.OAuth2;
-using Google.Apis.Drive.v3;
-using Google.Apis.Services;
-using Google.Apis.Util.Store;
-using Microsoft.Win32;
-using File = Google.Apis.Drive.v3.Data.File;
 
 namespace BenchmarkingCloudStorage
 {
+    public interface IClouds
+    {
+        void StartService();
+        void UploadFile(string filePath);
+        void ListFiles();
+    }
+
     class Program
     {
-        // If you modify these scopes, delete your previously saved credentials at ~/.credentials/drive-dotnet-quickstart.json
-        // Or it won't work :)
-        static string[] Scopes = { DriveService.Scope.Drive,
-                           DriveService.Scope.DriveAppdata,
-                           DriveService.Scope.DriveFile,
-                           DriveService.Scope.DrivePhotosReadonly,
-                           DriveService.Scope.DriveMetadataReadonly,
-                           DriveService.Scope.DriveReadonly,
-                           DriveService.Scope.DriveScripts };
-        static string ApplicationName = "Benchmarking Cloud Storage";
-
         static void Main(string[] args)
         {
-            // Create user credentials.
-            UserCredential credential;
+            // Number of files.
+            int n = 3;
 
-            using (var stream = new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
-            {
-                string credPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
-                credPath = Path.Combine(credPath, ".credentials/drive-dotnet-quickstart.json");
+            // Size of each file.
+            int k = 1;
 
-                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
-                    GoogleClientSecrets.Load(stream).Secrets,
-                    Scopes,
-                    "user",
-                    CancellationToken.None,
-                    new FileDataStore(credPath, true)).Result;
-                Console.WriteLine("Credential file saved to: " + credPath);
-            }
+            // m = MB; k = KB
+            char type = 'k';
 
-            // Create Drive API service.
-            var service = new DriveService(new BaseClientService.Initializer
-            {
-                HttpClientInitializer = credential,
-                ApplicationName = ApplicationName
-            });
+            GenerateLoad(n, k, type);
 
-            // File path to upload.
-            string filePath = "../../files/Konzultacije.jpg";
+            GoogleDrive gd = new GoogleDrive();
+            gd.StartService();
 
             DateTime t1 = DateTime.Now;
-            UploadFile(service, filePath);
+            for (var i = 0; i < n; i++)
+            {
+                gd.UploadFile($"out{i}.bin");    
+            }
             TimeSpan t = DateTime.Now - t1;
-
-            Console.WriteLine(t);
-            ListFiles(service);
-        }
-
-        // List all files on Drive.
-        private static void ListFiles(DriveService _service)
-        {
-            FilesResource.ListRequest listRequest = _service.Files.List();
-            listRequest.PageSize = 10;
-            listRequest.Fields = "nextPageToken, files(id, name)";
-            
-            IList<File> files = listRequest.Execute().Files;
-
-            Console.WriteLine("Files:");
-
-            if (files != null && files.Count > 0)
-            {
-                foreach (var f in files)
-                {
-                    Console.WriteLine("{0} ({1})", f.Name, f.Id);
-                }
-            }
-            else
-            {
-                Console.WriteLine("No files found.");
-            }
-            Console.Read();
-        }
-
-        // Get the mime type of the file. 
-        private static string GetMimeType(string fileName)
-        {
-            string mimeType = "application/unknown";
-            string ext = Path.GetExtension(fileName).ToLower();
-            RegistryKey regKey = Registry.ClassesRoot.OpenSubKey(ext);
-
-            if (regKey != null && regKey.GetValue("Content Type") != null)
-                mimeType = regKey.GetValue("Content Type").ToString();
-
-            return mimeType;
+           
+            Console.WriteLine("Time taken to upload {0} files each of size {1}{2}B: {3} s", n, k, type, t.TotalSeconds);
+            gd.ListFiles();
         }
         
-        // Upload file to Drive.
-        public static File UploadFile(DriveService _service, string _uploadFile)
+        // Generate n files, each file of size k
+        // type: m - MB, k - KB
+        private static void GenerateLoad(int n, int k, char type)
         {
-            if (System.IO.File.Exists(_uploadFile))
-            {
-                File body = new File();
-                body.Name = Path.GetFileName(_uploadFile);
-                body.Description = "Test upload";
-                body.MimeType = GetMimeType(_uploadFile);
-                
-                // File's content. 
-                byte[] byteArray = System.IO.File.ReadAllBytes(_uploadFile);
-                MemoryStream stream = new MemoryStream(byteArray);
+            var fileSize = (int)(Math.Pow(2, 20) / 4);
+            
+            if (type == 'k')
+                fileSize = (int)(Math.Pow(2, 10) / 4);
+            
+            var rand = new Random();
 
-                try
-                {
-                    FilesResource.CreateMediaUpload request = _service.Files.Create(body, stream, GetMimeType(_uploadFile));
-                    request.Upload();
-                    return request.ResponseBody;
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("An error occurred: " + e.Message);
-                    return null;
-                }
+            for (var i = 0; i < n; i++)
+            {
+                File.Delete($"out{i}.bin");
+                using (var writer = new BinaryWriter(File.Open($"out{i}.bin", FileMode.CreateNew)))
+                    for (var j = 0; j < fileSize*k; j++)
+                        writer.Write(rand.Next());
             }
-            Console.WriteLine("File does not exist: " + _uploadFile);
-            return null;
-        }     
+        }  
     }
 }
